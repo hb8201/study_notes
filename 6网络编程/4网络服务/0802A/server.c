f1e9dfd0c8bd7ecf74e26d76a69e6eb772869744
hb <hb@qq.com>
2026-08-03 20:37:49 +0800

    优化了http服务器去响应浏览器的资源申请，并且添加了一些常用资源的申请，学习了数据库的的基本操作

--- 6网络编程/4网络服务/0802A/server.c
+++ 6网络编程/4网络服务/0802A/server.c
@@ -7,13 +7,75 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 
-#define QQMAX 20
-
-typedef struct
+char *type(char *p)
 {
-    char ch[128];
-}  Qingqiu;
+    // 文本文档
+    if (strcmp(p, ".html") == 0)
+        return "text/html";
+    if (strcmp(p, ".css") == 0)
+        return "text/css";
+    if (strcmp(p, ".js") == 0 || strcmp(p, ".mjs") == 0)
+        return "text/javascript";
+    if (strcmp(p, ".txt") == 0 || strcmp(p, ".text") == 0)
+        return "text/plain";
+    if (strcmp(p, ".csv") == 0)
+        return "text/csv";
+    if (strcmp(p, ".doc") == 0)
+        return "application/msword";
+    if (strcmp(p, ".docx") == 0)
+        return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+    if (strcmp(p, ".xls") == 0)
+        return "application/vnd.ms-excel";
+    if (strcmp(p, ".xlsx") == 0)
+        return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    if (strcmp(p, ".ppt") == 0)
+        return "application/vnd.ms-powerpoint";
+    if (strcmp(p, ".pptx") == 0)
+        return "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+    if (strcmp(p, ".pdf") == 0)
+        return "application.pdf";
+    if (strcmp(p, ".rtf") == 0)
+        return "application.rtf";
+    // 图片与图形
+    if (strcmp(p, ".gif") == 0)
+        return "image/gif";
+    if (strcmp(p, ".ico") == 0)
+        return "image.vnd.microsoft.icon";
+    if (strcmp(p, ".jpeg") == 0 || strcmp(p, ".jpg") == 0)
+        return "image/jpeg";
+    if (strcmp(p, ".png") == 0)
+        return "image/png";
+    if (strcmp(p, ".bmp") == 0)
+        return "image/bmp";
+    if (strcmp(p, ".avif") == 0)
+        return "image/avif";
+    if (strcmp(p, ".svg") == 0)
+        return "image/svg+xml";
+    if (strcmp(p, ".tif") == 0 || strcmp(p, ".tiff") == 0)
+        return "image/tiff";
+    if (strcmp(p, ".webp") == 0)
+        return "image/webp";
+    // 音频与视频
+    if (strcmp(p, ".mp3") == 0)
+        return "audio/mpeg";
+    if (strcmp(p, ".wav") == 0)
+        return "audio/x-wav";
+    if (strcmp(p, ".mp4") == 0)
+        return "video/mp4";
+    if (strcmp(p, ".mpeg") == 0 || strcmp(p, ".mpg") == 0)
+        return "video/mpeg";
+    if (strcmp(p, ".avi") == 0)
+        return "video/x-msvideo";
+    if (strcmp(p, ".mov") == 0)
+        return "video/quicktime";
+    if (strcmp(p, ".ogg") == 0)
+        return "audio/ogg";
+    if (strcmp(p, ".ogv") == 0)
+        return "video/ogg";
+    return "application/octet-stream";
+}
 
 int main(int argc, char const *argv[])
 {
@@ -39,43 +101,67 @@ int main(int argc, char const *argv[])
         int len = sizeof(c_addr);
         int c_fd = accept(fd, (struct sockaddr *)&c_addr, &len);
 
-        char buf[1024] = {0};
+        char buf[2048] = {0};
         recv(c_fd, buf, sizeof(buf), 0);
         printf("%s", buf);
-        
-        Qingqiu qq[QQMAX] = {0};
-        int count = 0;
 
-        // 获取方法
-        char *ch1 = strchr(buf, ' ');
-        strncpy(qq[count].ch, buf, ch1 - buf);
-        printf("%s\n", qq[count].ch);
-        count++;
-        // 获取资源位置
-        char *ch2 = strchr(ch1 + 1, ' ');
-        strncpy(qq[count].ch, ch1 + 1, ch2 - ch1 - 1);
-        printf("%s\n", qq[count].ch);
-        count++;
-        // 获取版本
-        ch1 = strchr(buf, '\n');
-        strncpy(qq[count].ch, ch2 + 1, ch1 - ch2 - 1);
-        printf("%s\n", qq[count].ch);
-        count++;
+        char fangfa[20] = {0};
+        char lujin[50] = {0};
+        char banben[20] = {0};
 
-        char new_buf[1024];
-        strncpy(new_buf, ch1 + 1, &buf[strlen(buf)] - ch1);
-        // 获取首部字段
-        while (1)
+        sscanf(buf, "%s %s %s", fangfa, lujin, banben);
+
+        char ch1[1024] = {0};
+        char *ch2 = strchr(lujin, '.');
+        ch2 = type(ch2);
+        char msg[2048] = {0};
+
+        if (strcmp(ch2, "application/octet-stream") == 0)
         {
-            if (count >= QQMAX)
-            {
-                printf("首部字段过多，请扩大 QQMAX\n");
-                return 1;
-            }
-            
+            char msg1[] = "HTTP/1.1 404 NOT Found\r\n"
+                    "Content-Type: text/html\r\n"
+                    "\r\n"
+                    "<h1>404 NOT Found</h1>";
+            send(c_fd, msg1, strlen(msg1), 0);
         }
 
+        sprintf(ch1, "/var/www/html%s",lujin);
+        if (strstr(ch1, "..") != NULL)
+        {
+            snprintf(msg, sizeof(msg), 
+                "%s 403 Forbidden\r\n"
+                "Content-Type: %s \r\n\r\n"
+                "<h1>403 Forbidden</h1>", banben, ch2);
+            printf("%s", msg);
+            send(c_fd, msg, strlen(msg), 0);
+        }
+
+        int file_fd = open(ch1, O_RDONLY);
+        if (file_fd == -1)
+        {
+            snprintf(msg, sizeof(msg), 
+                "%s 404 Not Found\r\n"
+                "Content-Type: %s \r\n\r\n"
+                "<h1>404 Not Found</h1>", banben, ch2);
+            send(c_fd, msg, strlen(msg), 0);
+        }
+        else
+        {
+            snprintf(msg, sizeof(msg), 
+                "%s 200 OK\r\n"
+                "Content-Type: %s \r\n\r\n", banben, ch2);
+            send(c_fd, msg, strlen(msg), 0);
+
+            int n;
+            while ((n = read(file_fd, msg, sizeof(msg))) > 0)
+            {
+                send(c_fd, msg, n, 0);
+            }
+        }
+        close(file_fd); 
+        close(c_fd);
     }
+    close(fd);
     
     return 0;
 }
